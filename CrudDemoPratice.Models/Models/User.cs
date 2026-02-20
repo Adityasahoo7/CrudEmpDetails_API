ff489c040790f0854dbbad99be8f21000c8916ab
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-02-20 19:21:41 +0530

    Initial Add Authentcation

---
+++ CrudDemoPratice.Models/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudDemoPratice.Models.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "ID Must be Required")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username can't be longer than 50 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Role is required.")]
+        [RegularExpression("^(Admin|User|Manager)$", ErrorMessage = "Role must be Admin, User, or Manager.")]
+        public string Role { get; set; }
+
+    }
+}
